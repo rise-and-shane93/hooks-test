bc955c3559bdd1fa3968b2408b40001b9f38112c
Shane Harper <cdtharper@gmail.com>
2021-08-30 21:19:32 -0400

    initial changes after useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Todo from './components/Todo';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool to-do app",
+      isCompleted: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo}/>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function Todo({todo, index}) {
+
+    return (
+        <div className="todo">
+            {todo.text}
+        </div>
+    )
+}
+
+export default Todo;
